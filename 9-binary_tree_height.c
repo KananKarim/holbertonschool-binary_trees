dbd6d5ad2fa26937583a72d2104c0aa3371cde9a
KananKarim <kenankerimli7770@gmail.com>
2024-01-11 06:55:15 -0800

    fixing err

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh, rh;
+	size_t lh = 0, rh = 0;
 
 	if (!tree)
 		return (0);
 
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
+	lh = 1 + binary_tree_height(tree->left);
+	rh = 1 + binary_tree_height(tree->right);
 
-	return (lh > rh ? lh + 1 : rh + 1);
+	return (lh > rh ? lh : rh);
 }
