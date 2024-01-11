092dbadc5f0ea6072216e9b91130b6866e7fe7a4
KananKarim <kenankerimli7770@gmail.com>
2024-01-11 06:26:59 -0800

    post-order func

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - postorder func
+ * @tree: root
+ * @func: print_num
+ * Return: void
+ */
+
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+
+	func(tree->n);
+}
