c918da942888340c648a6a394e01389742d44016
KananKarim <kenankerimli7770@gmail.com>
2024-01-11 04:25:33 -0800

    system problem

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,8 +9,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	if (!parent)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 		return (NULL);
