404c4078faf3d2c9385d1d1156dc02b5035fccc5
KananKarim <kenankerimli7770@gmail.com>
2024-01-11 03:48:42 -0800

    insert node left of parent

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert node to the left of par
+ * @parent: parent node
+ * @value: value of new node
+ * Return: pointer to new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+
+	parent->left = new_node;
+
+	return (new_node);
+}
