24474c165074a9f380be94aaa7f92b46ddfcd15f
MrNeonn75 <devneonn75@gmail.com>
2024-01-14 22:34:08 +0400

    16-binary_tree_is_perfect bugfix fixing binary_tree_is_perfect function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,48 +1,44 @@
 #include "binary_trees.h"
+
+
 /**
  * _binary_tree_height - local function.
  * @tree: A pointer to the root node of the tree to check.
+ * 
  * Return: Data.
  */
 size_t _binary_tree_height(const binary_tree_t *tree)
 {
-	int r_height, l_height;
+        int r_height, l_height;
 
-	if (!tree)
-		return (0);
-	r_height = _binary_tree_height(tree->right);
-	l_height = _binary_tree_height(tree->left);
-	return (1 + ((r_height >= l_height) ? r_height : l_height));
+        if (!tree)
+                return (0);
+        r_height = _binary_tree_height(tree->right);
+        l_height = _binary_tree_height(tree->left);
+        return (1 + ((r_height >= l_height) ? r_height: l_height));
 }
 
 /**
  * _binary_tree_balance - local function.
  * @tree: A pointer to the root node of the tree to check.
+ * 
  * Return: Data.
  */
 int _binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
+        if (!tree)
+                return (0);
+        return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
 
 /**
- * sub_tree_perfect - The function which we use for
- * @tree: A pointer to the root node of the tree to check
+ * sub_tree_perfect - The function which we use for binary_tree_is_perfect function.
+ * @tree: A pointer to the root node of the tree to check.
+ * 
  * Return: 1 or 0.
  */
-
 int sub_tree_perfect(const binary_tree_t *tree)
 {
-<<<<<<< HEAD
-	if (tree && !tree->right & !tree->left)
-		return (1);
-	if (tree && tree->right && tree->left)
-		return (1 && sub_tree_perfect(tree->left)
-				&& sub_tree_perfect(tree->right));
-	return (0);
-=======
         if (tree && !tree->right && !tree->left)
                 return (1);
 
@@ -50,7 +46,6 @@ int sub_tree_perfect(const binary_tree_t *tree)
                 return (1 && sub_tree_perfect(tree->left)
                 && sub_tree_perfect(tree->right));
         return (0);
->>>>>>> 3a063416d846ab7efeb975c3e12a7ecc89b113cc
 }
 
 /**
@@ -62,10 +57,13 @@ int sub_tree_perfect(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	if (_binary_tree_balance(tree) != 0)
-		return (0);
-	return (sub_tree_perfect(tree->left) && sub_tree_perfect(tree->right));
-}
+    if (!tree)
+        return (0);
 
+    if (_binary_tree_balance(tree) != 0)
+        return (0);
+
+    return (sub_tree_perfect(tree) &&
+            binary_tree_is_perfect(tree->left) &&
+            binary_tree_is_perfect(tree->right));
+}
