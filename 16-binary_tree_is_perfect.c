12d0e977b46f3974f7a29b1bcc74349c37e6bb38
MrNeonn75 <devneonn75@gmail.com>
2024-01-14 22:45:26 +0400

    16-binary_tree_is_perfect bugfix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -26,9 +26,10 @@ size_t _binary_tree_height(const binary_tree_t *tree)
  */
 int _binary_tree_balance(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
-        return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
+    if (!tree)
+        return (0);
+
+    return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
 
 /**
