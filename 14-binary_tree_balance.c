17312a845a5a5fa883b4fc7955b5a48669a7636d
KananKarim <kenankerimli7770@gmail.com>
2024-01-14 10:21:00 -0800

    betty fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,16 +12,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t l = 0, r = 0;
 
-        if (tree->left)
-            l = 1 + binary_tree_height(tree->left);
-        else
-            l = 1;
-
-        if (tree->right)
-            r = 1 + binary_tree_height(tree->right);
-        else
-            r = 1;
-
+		if (tree->left)
+			l = 1 + binary_tree_height(tree->left);
+		else
+			l = 1;
+		if (tree->right)
+			r = 1 + binary_tree_height(tree->right)
+		else
+			r = 1;
 		return ((l > r) ? l : r);
 	}
 	return (0);
@@ -36,8 +34,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-
-    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (!tree)
+		return (0);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
