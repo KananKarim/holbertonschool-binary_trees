5680c0f358889272ca7909b9e2220bd0fafb0475
KananKarim <kenankerimli7770@gmail.com>
2024-01-14 11:35:31 -0800

    betty fixed

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,15 +9,13 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-        binary_tree_t *uncle;
+	binary_tree_t *uncle;
 
-        if (!node || !node->parent || !node->parent->parent
-        || !node->parent->parent->left
-        || !node->parent->parent->right)
-                return (0);
-
-        uncle = node->parent->parent->right;
-        uncle = (node->parent == uncle) ? node->parent->parent->left : uncle;
-
-        return uncle;
+	if (!node || !node->parent || !node->parent->parent
+			|| !node->parent->parent->left
+			|| !node->parent->parent->right)
+		return (0);
+	uncle = node->parent->parent->right;
+	uncle = (node->parent == uncle) ? node->parent->parent->left : uncle;
+	return (uncle);
 }
